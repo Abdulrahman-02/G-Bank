669af3a20a0457526f00dbd089277e7439647bea
abdulrahman-02 <abderrahmanebenaissa740@gmail.com>
2023-08-26 23:49:53 +0100

    implemented RESTful API using Gin

---
+++ api/server.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	db "github.com/abdulrahman-02/G-Bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking service.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing.
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+	router.PUT("/accounts", server.updateAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ docs/11- Implement RESTful  API.md
@@ -0,0 +1,18 @@
+# Implement RESTful HTTP API in Go using Gin
+
+## Popular web frameworks in Go
+
+- Gin
+- Beego
+- Echo
+- Revel
+- Martini
+- Fiber
+- Buffalo
+
+## Popular HTTP routers in Go
+
+- FastHTTP
+- Gorilla Mux
+- HttpRouter
+- Chi
